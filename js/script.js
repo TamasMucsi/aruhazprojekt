885f2616f1e1d8587bb92baa734045bd886fc942
TamasMucsi <mucsi.tamas-2023@keri.mako.hu>
2026-03-17 09:55:43 +0100

    betoltesx

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let cipok = [];
 let szurescipok = [];
 
 
+let cipokcardok = document.getElementById("cipokcardok")
+
 
 
 
@@ -10,14 +12,40 @@ async function cipokBetoltes() {
     try {
         const response = await fetch('./json/webaruhaz.json');
         cipok = await response.json();
-        szurescipokcipok = [...cipok];
+        szurescipok = [...cipok];
         cipokmegjelenitese();
         kategoriaMenuGeneralas();
+        console.log('Hibasssssssa történt a filmek betöltése során!')
     } catch (error) {
-        console.log('Hiba történt a filmek betöltése során!')
+        console.log('Hiba történt a cipők betöltése során!')
     }
 }
 
 function cipokmegjelenitese(){
-    const card = document.createElement()
+    cipokcardok.innerHTML = "";
+    
+    szurescipok.forEach(element => {
+        const card = document.createElement("div");
+        card.classList = "col-sm-12 col-md-6 col-lg-3"
+        card.innerHTML = `
+            <div class="card" style="width: 20rem;">
+                <img src="${element.kep}" class="card-img-top" alt="${element.kep}">
+                <div class="card-body">
+                    <h4 class="card-title">${element.marka}</h4>
+                    <h6 class="fw-bold">${element.modell}</h6>
+                    <p class="card-text">Szín: ${element.szin} | Méret: ${element.meret}</p>
+                    <p class="card-text">Típus: ${element.tipus}</p>
+
+                    <h5 class="fw-bold">${element.ar} Ft</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>`
+        cipokcardok.appendChild(card)
+    });
+    
 }
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    cipokBetoltes();
+})
