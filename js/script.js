ee8120048a1d84be9b1000804e772c46e8bde89a
vinczebalazs12 <vincze.balazs-2023@keri.mako.hu>
2026-03-18 11:54:42 +0100

    navbar footer külső

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// 1. JS - navbar, footer behúzás
+
+function includeHtml(name) {
+    fetch(`./${name}.html`)
+        .then(response => response.text())
+        .then(html => document.querySelector(name).innerHTML = html);
+}
+
+
+
 let cipok = [];
 let szurescipok = [];
 let kosarcipok = [];
